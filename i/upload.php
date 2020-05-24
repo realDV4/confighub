8b14ae03e4aa8fa9fdf7b78a405f8a358b63b2a7
realDV4 <64649255+realDV4@users.noreply.github.com>
2020-05-24 10:14:06 +0200

    Add files via upload

---
+++ i/upload.php
@@ -0,0 +1,24 @@
+<?php
+header("Content-Type: text/text");
+$key = "JFuthutgh287tg3287th2387th3276t";
+$uploadhost = "http://confighub.eu/i/";
+$redirect = "http://confighub.eu";
+if ($_SERVER["REQUEST_URI"] == "/robot.txt") { die("User-agent: *\nDisallow: /"); }
+if (isset($_POST['k'])) {
+    if ($_POST['k'] == $key) {
+        $target = getcwd() . "/" . basename($_FILES['d']['name']);
+        if (move_uploaded_file($_FILES['d']['tmp_name'], $target)) {
+            $md5 = md5_file(getcwd() . "/" . basename($_FILES['d']['name']));
+			$explode = explode(".", $_FILES["d"]["name"]);
+            rename(getcwd() . "/" . basename($_FILES['d']['name']), getcwd() . "/" . $md5 . "." . end($explode));
+            echo $uploadhost . $md5 . "." . end($explode);
+        } else {
+            echo "Sorry, there was a problem uploading your file.";
+        }
+    } else {
+        header('Location: '.$redirect);
+    }
+} else {
+    header('Location: '.$redirect);
+}
+?>
