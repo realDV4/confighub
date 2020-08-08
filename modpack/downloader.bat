88336b7c36e7ae5bb980007c918a70cdb7d464ac
realDV4 <64649255+realDV4@users.noreply.github.com>
2020-08-08 10:55:36 +0200

    Update downloader.bat

--- modpack/downloader.bat
+++ modpack/downloader.bat
@@ -1,3 +1,3 @@
-echo off
-echo dv4 gamer!?!?!?!?!?
+@echo off
+bitsadmin /transfer myDownloadJob /download /priority normal https://github.com/realDV4/confighub/raw/gh-pages/modpack/armorexpansion-1.12.2.2.jar %appdata%\.minecraft\mods\1.12.2\
 pause
