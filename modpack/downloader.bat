8a2be44cf86136e10c0ec9e11581c2c0c9b8c94b
realDV4 <64649255+realDV4@users.noreply.github.com>
2020-08-12 12:46:01 +0200

    Update downloader.bat

--- modpack/downloader.bat
+++ modpack/downloader.bat
@@ -12,18 +12,12 @@ IF %M%==1 GOTO optifine
 IF %M%==2 GOTO mods
 :mods
 md %appdata%\.minecraft\mods\1.12.2
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/adminshop-1.5.1.jar %appdata%\.minecraft\mods\1.12.2\adminshop-1.5.1.jar
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/JurassiCraft-1.12.2-2.1.23.jar %appdata%\.minecraft\mods\1.12.2\JurassiCraft-1.12.2-2.1.23.jar
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/[1.12.2]+SecurityCraft+v1.8.19.3.jar %appdata%\.minecraft\mods\1.12.2\[1.12.2]+SecurityCraft+v1.8.19.3.jar
 bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/armorexpansion-1.12.2.2.jar %appdata%\.minecraft\mods\1.12.2\armorexpansion-1.12.2.2.jar
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/furniture-6.3.1-1.12.2.jar %appdata%\.minecraft\mods\1.12.2\furniture-6.3.1-1.12.2.jar
 bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/jei_1.12.2-4.15.0.296.jar %appdata%\.minecraft\mods\1.12.2\jei_1.12.2-4.15.0.296.jar
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/llibrary-1.7.19-1.12.2.jar %appdata%\.minecraft\mods\1.12.2\llibrary-1.7.19-1.12.2.jar
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/malisiscore-1.12.2-6.5.1.jar %appdata%\.minecraft\mods\1.12.2\malisiscore-1.12.2-6.5.1.jar
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/malisisdoors-1.12.2-7.3.0.jar %appdata%\.minecraft\mods\1.12.2\malisisdoors-1.12.2-7.3.0.jar
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/omlib-1.12.2-3.1.4-249.jar %appdata%\.minecraft\mods\1.12.2\omlib-1.12.2-3.1.4-249.jar
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/openmodularturrets-1.12.2-3.1.11-372.jar %appdata%\.minecraft\mods\1.12.2\openmodularturrets-1.12.2-3.1.11-372.jar
-bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/lootcrate-1.12.2-1.0.1.jar %appdata%\.minecraft\mods\1.12.2\lootcrate-1.12.2-1.0.1.jar
+bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/obfuscate-0.2.6-1.12.2.jar %appdata%\.minecraft\mods\1.12.2\obfuscate-0.2.6-1.12.2.jar
+bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/cgm-0.15.3-mc1.12.2.jar %appdata%\.minecraft\mods\1.12.2\cgm-0.15.3-mc1.12.2.jar
+bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/cfm-6.3.0-mc1.12.2.jar %appdata%\.minecraft\mods\1.12.2\cfm-6.3.0-mc1.12.2.jar
+bitsadmin /transfer Mods /download /priority normal https://vps.confighub.eu/cdm-0.4.1-mc1.12.2.jar %appdata%\.minecraft\mods\1.12.2\cdm-0.4.1-mc1.12.2.jar
 goto end
 del %temp%\dv4isgamer.bat /f 
 :forge
